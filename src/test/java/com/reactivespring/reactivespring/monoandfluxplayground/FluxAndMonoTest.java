095e85f323557c8eb6e85732601e0066ba5a293d
facova <fsodilao@gmail.com>
2021-02-07 00:48:36 -0300

    Junit with reactive

---
+++ src/test/java/com/reactivespring/reactivespring/monoandfluxplayground/FluxAndMonoTest.java
@@ -0,0 +1,76 @@
+package com.reactivespring.reactivespring.monoandfluxplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class FluxAndMonoTest {
+
+    @Test
+    public void fluxTest() {
+
+        Flux<String> stringFlux = Flux.just("Tatiane", "É", "Feia")
+                .concatWith(Flux.error(new RuntimeException("Erro Aconteceu")))
+                .concatWith(Flux.just("Cabou aqui"));
+
+        stringFlux
+                .subscribe(System.out::println,
+                        (e) -> System.err.println(e),
+                        () -> System.out.println("Cabou o programa, acabou"));
+    }
+
+    @Test
+    public void shouldVerifyWithoutError() {
+
+        Flux<String> stringFlux = Flux.just("Tatiane", "É", "Feia");
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Tatiane")
+                .expectNext("É")
+                .expectNext("Feia")
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void shouldVerifyWithError() {
+
+        Flux<String> stringFlux = Flux.just("Tatiane", "É", "Feia")
+                .concatWith(Flux.error(new RuntimeException("Erro Aconteceu")));
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Tatiane")
+                .expectNext("É")
+                .expectNext("Feia")
+                .expectError(RuntimeException.class)
+                .verify();
+
+    }
+
+    @Test
+    public void shouldVerifyWithErrorCunt() {
+
+        Flux<String> stringFlux = Flux.just("Tatiane", "É", "Feia")
+                .concatWith(Flux.error(new RuntimeException("Erro Aconteceu")));
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(3)
+                .expectError(RuntimeException.class)
+                .verify();
+
+    }
+
+    @Test
+    public void shouldVerifyWithErrorWithOneNextLine() {
+
+        Flux<String> stringFlux = Flux.just("Tatiane", "É", "Feia")
+                .concatWith(Flux.error(new RuntimeException("Erro Aconteceu")));
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Tatiane", "É", "Feia")
+                .expectError(RuntimeException.class)
+                .verify();
+
+    }
+
+}
